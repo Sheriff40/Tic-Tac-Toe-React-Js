2f30c2aa013cffbe74b326374999058d41d6f4f3
Sumin James <suminjames6@gmail.com>
2019-09-17 14:09:11 +0545

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Board extends React.Component{
+    renderSquare(){
+        return <Square/>
+    }
+    render(){
+        // const status = this.props.value;
+        return(
+            <div>
+                {console.log("hi")}
+                <div>Next player {this.props.value}</div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                    {this.renderSquare()}
+                </div>
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Game extends React.Component{
+    render(){
+        return(
+            <div>
+                <Board/>
+            </div>
+        )
+    }
+}
+
+class Square extends React.Component{
+    constructor(){
+        super();
+        this.state = {value: "X"}
+    }
+
+    changeState(){
+        // this.setState({value:"X"})
+        return(
+            <div>
+                <Board status={this.state.value}/>
+                {console.log(this.state.value)}
+            </div>
+
+        )
+    }
+    render(){
+        return(
+            <button className="square" onClick={()=>this.changeState()}>{this.state.value}</button>
+        )
+    }
+}
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
